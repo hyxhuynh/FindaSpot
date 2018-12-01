3847ca5140c6b3967419e014bd4715f1b3336369
Amber <amber.r.struckman@gmail.com>
2018-12-01 11:55:57 -0800

    adding the untracked files

---
+++ routes/authRoutes.js
@@ -0,0 +1,34 @@
+
+// function signUp(req, res) {
+//     res.render('signup');
+// }
+
+// function signIn(req, res) {
+//     res.rendfer('signin');
+// }
+
+function logOut(req, res) {
+    req.session.destroy(function(err) {
+        res.redirect('/');
+    });
+}
+
+function auth(req, res) {
+    res.render('auth', {});
+}
+
+module.exports = function(app, passport) {
+    app.get('/auth', auth);
+    // app.get('/signup', signUp);
+    // app.get('/signin', signIn);
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/',
+        failureRedirect: '/auth#signup'
+    }));
+    app.post('/signin', passport.authenticate('local-signin', {
+            successRedirect: '/',
+            failureRedirect: '/auth#signin'
+    }
+    ));
+    app.get('/logout', logOut);
+}
