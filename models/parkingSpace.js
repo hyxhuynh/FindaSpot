d85fba093e229ba92ea1a342e266c6c2959ccbcd
M. Pennella <michael.pennella@gmail.com>
2018-12-01 01:32:17 -0800

    Added more fields to ParkingSpace model

--- models/parkingSpace.js
+++ models/parkingSpace.js
@@ -1,6 +1,17 @@
 module.exports = function(sequelize, DataTypes) {
     var ParkingSpace = sequelize.define("ParkingSpace", {
-        text: DataTypes.STRING,
+        // Owner corresponds to user ID
+        owner: DataTypes.INTEGER,
+
+        // Location by address string and lat/long
+        address: DataTypes.STRING,
+        latitude: DataTypes.DOUBLE,
+        longitude: DataTypes.DOUBLE,
+
+        // Particulars of space
+        spaceSize: DataTypes.ENUM("standard","compact","motorcycle","rv"),
+        spaceCover: DataTypes.ENUM("uncovered","covered","garage"),
+
         description: DataTypes.TEXT
     });
     return ParkingSpace;
