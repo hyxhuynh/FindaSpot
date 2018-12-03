93917479218f6b345f7f90e889307a753fb4c929
M. Pennella <michael.pennella@gmail.com>
2018-12-03 15:47:01 -0800

    Added validation to ParkingSpace lat/long for min and max values

--- models/parkingSpace.js
+++ models/parkingSpace.js
@@ -5,8 +5,16 @@ module.exports = function(sequelize, DataTypes) {
 
         // Location by address string and lat/long
         address: DataTypes.STRING,
-        latitude: DataTypes.DOUBLE,
-        longitude: DataTypes.DOUBLE,
+        latitude: {
+            type: DataTypes.DOUBLE,
+            // Min of -90 degrees/max of 90 degrees
+            validate: { min: -90, max: 90 }
+        },
+        longitude: {
+            type: DataTypes.DOUBLE,
+            // Min of -180 degrees/max of 180 degrees
+            validate: { min: -180, max: 180 }
+        },
 
         // Particulars of space
         spaceSize: DataTypes.ENUM("standard","compact","motorcycle","rv"),
